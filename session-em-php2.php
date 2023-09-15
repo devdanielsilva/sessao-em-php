52675ce867d62787ebb5c7e96ad0a80ac2513f99
Daniel Silva <devdanielsilva@gmail.com>
2023-09-15 20:17:26 -0300

    .

--- session-em-php2.php
+++ session-em-php2.php
@@ -7,14 +7,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teste de Sessão em PHP</title>
+    <title>Document</title>
 </head>
 <body>
-    <?php
-    $_SESSION ["favcolor"] = "Red";
-    $_SESSION ["favanimal"] = "Dog";
-    $_SESSION ["time"] = "São Paulo Fc";
-    echo "As variáveis de sessão estão definidas.";
-    ?>
+    
 </body>
 </html>
