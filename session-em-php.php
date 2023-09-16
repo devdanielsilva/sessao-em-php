398461629ac797e7d4e87a60ee6fd38ef1000a2c
Daniel Silva <devdanielsilva@gmail.com>
2023-09-16 15:39:10 -0300

    Update session-em-php.php

--- session-em-php.php
+++ session-em-php.php
@@ -1,24 +1,29 @@
 <?php
-    //Inicia a sessão
-    //A função session_start(); deve ser a primeira coisa no seu documento. Antes de qualquer tag HTML.
-    session_start();
+//Inicia a sessão
+//A função session_start(); deve ser a primeira coisa no seu documento. Antes de qualquer tag HTML.
+session_start();
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teste de Sessão em PHP</title>
 </head>
+
 <body>
     <button><a href="../sessao-em-php2.php/">Sessão 2 </a></button>
+
     <?php
     //Defini variáveis de sessão
-    $_SESSION ["favcolor"] = "Red";
-    $_SESSION ["favanimal"] = "Dog";
-    $_SESSION ["time"] = "São Paulo Fc";
+    $_SESSION["favcolor"] = "Red";
+    $_SESSION["favanimal"] = "Dog";
+    $_SESSION["time"] = "São Paulo Fc";
     echo "As variáveis de sessão estão definidas.";
     ?>
 </body>
+
 </html>
